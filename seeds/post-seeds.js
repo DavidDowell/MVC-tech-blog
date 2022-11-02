d82c99aff6638bd221ab5bdc421f2b0c236762e3
DavidDowell <daviddowell1@gmail.com>
2022-11-02 17:34:07 -0400

    Updated Readme.md and created new comment page

--- seeds/post-seeds.js
+++ seeds/post-seeds.js
@@ -2,23 +2,23 @@ const { Post } = require('../models');
 
 const postdata = [
   {
-    title: 'Donec posuere metus vitae ipsum.',
-    post_content: 'https://buzzfeed.com/in/imperdiet/et/commodo/vulputate.png',
+    title: 'Yar Pirate Ipsum',
+    post_content: 'Deadlights jack lad schooner scallywag dance the hempen jign',
     user_id: 2
   },
   {
-    title: 'Morbi non quam nec dui luctus rutrum.',
-    post_content: 'https://nasa.gov/donec.json',
+    title: 'Monkey D. Luffy',
+    post_content: 'Jolly boat chase guns ahoy ye poop deck ballast nipperkin sheet jack transom. ',
     user_id: 3
   },
   {
-    title: 'Donec diam neque, vestibulum eget, vulputate ut, ultrices vel, augue.',
-    post_content: 'https://europa.eu/parturient/montes/nascetur/ridiculus/mus/etiam/vel.aspx',
+    title: 'The Once Piece',
+    post_content: 'Barbary Coast Pieces of Eight knave belaying pin Sea Legs nipperkin square-rigged.',
     user_id: 4
   },
   {
-    title: 'Nunc purus.',
-    post_content: 'http://desdev.cn/enim/blandit/mi.jpg',
+    title: 'WWW',
+    post_content: 'Admiral of the Black lanyard spike belaying pin fluke pirate sloop handsomely.',
     user_id: 1
   },
 ];
